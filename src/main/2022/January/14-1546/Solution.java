3cbda36c4f19baa9afcaa4de53d4487d9e13f5f8
weixliu <weixliu0801@gmail.com>
2022-01-14 23:45:02 +0800

    add solution

---
+++ src/main/2022/January/14-1546/Solution.java
@@ -0,0 +1,25 @@
+class Solution {
+	    public int maxNonOverlapping(int[] nums, int target) {
+		            int presum = 0;
+			            Map<Integer, Integer> sumMap = new HashMap<>();
+				            
+				            sumMap.put(0, 1);
+					            
+					            int res = 0;
+						            for(int i = 0;i < nums.length;i++) {
+								                presum += nums[i];
+										            
+										            int fKey = presum - target;
+											                if(sumMap.containsKey(fKey)) {
+														                res++;
+																                sumMap.clear();
+																		                sumMap.put(0, 1);
+																				                presum = 0;
+																						            } else {
+																								                    sumMap.put(presum, sumMap.getOrDefault(presum, 0) + 1);
+																										                }
+													        }
+							            
+							            return res;
+								        }
+}
