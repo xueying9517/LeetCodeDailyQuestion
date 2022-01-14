b0c197fd9eae11e3fc98c6de71429809dd1aade0
xueying9517 <xueying9517@qq.com>
2022-01-14 17:27:20 +0800

    add solution 1546

--- src/main/2022/January/14-1546/SolutionXueYing1546.java
+++ src/main/2022/January/14-1546/SolutionXueYing1546.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class SolutionXueYing1546 {
+import java.util.*;
+public class SolutionXueYing1546 {
+    public int maxNonOverlapping(int[] nums, int target) {
+        Set<Integer> set = new HashSet<>();
+        set.add(0);
+        int res = 0, sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (set.contains(sum - target)) {
+                res++;
+                set.clear();
+                sum = 0;
+                set.add(0);
+            } else {
+                set.add(sum);
+            }
+        }
+        return res;
+    }
 }
